0e5d42a24dd3dfc82a2f7775bfa242c55f07bfcb
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-10-13 10:27:22 +0200

    feat: add boolean examples

--- examples/booleans/test_booleans.py
+++ examples/booleans/test_booleans.py
@@ -1,3 +1,4 @@
+import pytest
 from expects import be_false, be_true, expect
 
 from booleans import (
@@ -11,37 +12,53 @@ from booleans import (
 
 
 class TestBooleans:
-    def test_is_truthy(self):
-        expect(is_truthy(True)).to(be_true)
-        expect(is_truthy(1)).to(be_true)
-        expect(is_truthy("Hello")).to(be_true)
-        expect(is_truthy([1, 2, 3])).to(be_true)
+    @pytest.mark.parametrize(
+        "value",
+        [
+            True,
+            1,
+            "Hello",
+            [1, 2, 3],
+        ],
+    )
+    def test_is_truthy(self, value):
+        expect(is_truthy(value)).to(be_true)
 
-    def test_is_falsy(self):
-        expect(is_truthy(False)).to(be_false)
-        expect(is_truthy(0)).to(be_false)
-        expect(is_truthy("")).to(be_false)
-        expect(is_truthy([])).to(be_false)
-        expect(is_truthy(None)).to(be_false)
+    @pytest.mark.parametrize(
+        "value",
+        [
+            False,
+            0,
+            "",
+            [],
+            None,
+        ],
+    )
+    def test_is_falsy(self, value):
+        expect(is_truthy(value)).to(be_false)
 
     def test_is_list_empty(self):
         expect(is_list_empty([])).to(be_true)
 
-    def test_is_list_not_empty(self):
         expect(is_list_empty([1, 2, 3])).to(be_false)
 
     def test_is_even(self):
         expect(is_even(4)).to(be_true)
+
         expect(is_even(5)).to(be_false)
 
     def test_has_permission(self):
         expect(has_permission(True, False)).to(be_false)
+
         expect(has_permission(True, True)).to(be_true)
+
         expect(has_permission(False, False)).to(be_false)
 
     def test_can_access_feature(self):
         expect(can_access_feature(True, 21)).to(be_true)
+
         expect(can_access_feature(False, 10)).to(be_false)
+
         expect(can_access_feature(True, 17)).to(be_true)
 
     def test_custom_object_truthiness(self):
